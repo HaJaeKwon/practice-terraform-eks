4c0ae1353875def6f795d3c3f257c4626e5d738d
하재권 <hazxz@naver.com>
2020-09-03 00:03:05 +0900

    feat: cluster security_group_rule 추가 (worker 노드와 연동)

--- modules/projects/eks/cluster-sg.tf
+++ modules/projects/eks/cluster-sg.tf
@@ -14,3 +14,13 @@ resource "aws_security_group" "eks_cluster" {
     Name = "${var.cluster_name}-cluster"
   }
 }
+
+resource "aws_security_group_rule" "eks_cluster_ingress_node_https" {
+  description = "Allow pods to communicate with the cluster API server"
+  from_port = 443
+  protocol = "tcp"
+  security_group_id = aws_security_group.eks_cluster.id
+  source_security_group_id = aws_security_group.eks_worker.id
+  to_port = 443
+  type = "ingress"
+}
